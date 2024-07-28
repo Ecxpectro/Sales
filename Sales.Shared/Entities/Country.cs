68165837c2c375c8d6993a218e5800b98048e089
Ecxpectro <henriquescunha12345@gmail.com>
2024-07-28 11:31:45 -0300

    Add data context

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name = "Country")]
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [MaxLength(100, ErrorMessage = "The field {0} has a maximum length of {1} characters.")]
+        public string Name { get; set; } = null!;
+    }
+}
