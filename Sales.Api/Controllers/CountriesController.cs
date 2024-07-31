4dc8f8b845773fe52affc450f9ea670514e06208
Ecxpectro <henriquescunha12345@gmail.com>
2024-07-31 14:50:26 -0300

    Completed countries Api controller

--- Sales.Api/Controllers/CountriesController.cs
+++ Sales.Api/Controllers/CountriesController.cs
@@ -5,27 +5,57 @@ using Sales.Shared.Entities;
 
 namespace Sales.Api.Controllers
 {
-	[ApiController]
-	[Route("/api/countries")]
-	public class CountriesController : ControllerBase
-	{
-		private readonly DataContext _context;
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
 
-		public CountriesController(DataContext context)
+        public CountriesController(DataContext context)
         {
-			_context = context;
-		}
-		[HttpGet]
-		public async Task<IActionResult> GetAsync()
-		{
-			return Ok(await _context.Countries.ToListAsync());
-		}
-		[HttpPost]
-		public async Task<ActionResult> PostAsync(Country country)
-		{
-			 _context.Countries.Add(country);
-			await _context.SaveChangesAsync();
-			return Ok(country);
-		}
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var coutry = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (coutry == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(coutry);
+        }
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Countries.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var coutry = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (coutry == null)
+            {
+                return NotFound();
+            }
+            _context.Countries.Remove(coutry);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
---
+++ Sales.WEB/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+
+namespace Sales.WEB.Repositories
+{
+	public class HttpResponseWrapper<T>
+	{
+		public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+		{
+			Error = error;
+			Response = response;
+			HttpResponseMessage = httpResponseMessage;
+		}
+
+		public bool Error { get; set; }
+
+		public T? Response { get; set; }
+
+		public HttpResponseMessage HttpResponseMessage { get; set; }
+
+		public async Task<string?> GetErrorMessageAsync()
+		{
+			if (!Error)
+			{
+				return null;
+			}
+
+			var statusCode = HttpResponseMessage.StatusCode;
+			if (statusCode == HttpStatusCode.NotFound)
+			{
+				return "Resource not found";
+			}
+			else if (statusCode == HttpStatusCode.BadRequest)
+			{
+				return await HttpResponseMessage.Content.ReadAsStringAsync();
+			}
+			else if (statusCode == HttpStatusCode.Unauthorized)
+			{
+				return "You must log in to perform this operation.";
+			}
+			else if (statusCode == HttpStatusCode.Forbidden)
+			{
+				return "You do not have permissions to perform this operation";
+			}
+
+			return "An unexpected error has occurred";
+		}
+	}
+
+}
