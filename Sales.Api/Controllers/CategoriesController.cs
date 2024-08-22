7c23a25e87b55e79df92757c6d775a986b521aff
Ecxpectro <henriquescunha12345@gmail.com>
2024-08-22 13:59:56 -0300

    Category api methods

---
+++ Sales.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.Api.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/categories")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Categories.ToListAsync());
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Category category)
+        {
+            try
+            {
+                _context.Categories.Add(category);
+                await _context.SaveChangesAsync();
+                return Ok(category);
+            }
+            catch (DbUpdateException dbupdateException)
+            {
+                if (dbupdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("There's a category with the same name.");
+                }
+
+                return BadRequest(dbupdateException.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Category category)
+        {
+            try
+            {
+                _context.Categories.Update(category);
+                await _context.SaveChangesAsync();
+                return Ok(category);
+            }
+            catch (DbUpdateException dbupdateException)
+            {
+                if (dbupdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("There's a category with the same name.");
+                }
+
+                return BadRequest(dbupdateException.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
