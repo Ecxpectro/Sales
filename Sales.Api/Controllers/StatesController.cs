89d329bf7567cb5a1d8d54694889645262e2b221
Ecxpectro <henriquescunha12345@gmail.com>
2024-08-24 10:56:50 -0300

    Creating state api controller

---
+++ Sales.Api/Controllers/StatesController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.Api.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/states")]
+    public class StatesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public StatesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.States.Include(x => x.Cities).ToListAsync());
+        }
+        
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var state = await _context.States.FirstOrDefaultAsync(x => x.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(state);
+        }
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(State state)
+        {
+            try
+            {
+                _context.States.Add(state);
+                await _context.SaveChangesAsync();
+                return Ok(state);
+            }
+            catch (DbUpdateException dbupdateException)
+            {
+                if (dbupdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("There's a state with the same name.");
+                }
+
+                return BadRequest(dbupdateException.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(State state)
+        {
+            try
+            {
+                _context.States.Update(state);
+                await _context.SaveChangesAsync();
+                return Ok(state);
+            }
+            catch (DbUpdateException dbupdateException)
+            {
+                if (dbupdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("There's a state with the same name.");
+                }
+
+                return BadRequest(dbupdateException.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var state = await _context.States.FirstOrDefaultAsync(x => x.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            _context.States.Remove(state);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
